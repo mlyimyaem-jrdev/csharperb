b79feb69ca4fef32c409f7aa2df1c76fffed509c
marcoyimyaem <marcoyimyaem@gmail.com>
2025-10-20 01:52:08 +0800

    Day 2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome Devs");
+        int age = 26;
+        System.Console.WriteLine("my age is" + age);
+        float cent = 0.76f;
+        double price = 21.09;
+        char letter = '!';
+        byte p = 1; //0-255
+        long trillions = 9999999999;
+        System.Console.WriteLine("Price is " + price);
+        string name = "Marc Yim";
+        bool isRunning = true;
+        System.Console.WriteLine(name + isRunning);
+        // System.Console.WriteLine(name + isRunning);
+        string _fullName = "";
+        System.Console.WriteLine("10 + 3" + (10 + 3));
+        System.Console.WriteLine("10 - 3" + (10 - 3));
+        System.Console.WriteLine("10 * 3" + (10 * 3));
+        System.Console.WriteLine("10 / 3" + (10 / 3));
+        System.Console.WriteLine("10 % 3" + (10 % 3));
+        System.Console.WriteLine("10 < 3" + (10 < 3));
+        System.Console.WriteLine("10 > 3" + (10 > 3));
+        System.Console.WriteLine("1 <= 1" + (1 <= 1));
+        System.Console.WriteLine("3 >= 3" + (3 >= 3));
+        System.Console.WriteLine("1 == 1" + (1 == 1));
+        System.Console.WriteLine("2 != 3" + (2 != 3));
+
+        System.Console.WriteLine("true && true" + (true && true));
+        System.Console.WriteLine("false && true" + (false && true));
+        System.Console.WriteLine("true && false" + (true && false));
+        System.Console.WriteLine("false && false" + (false && false));
+        
+        System.Console.WriteLine("true || true" + (true || true));
+        System.Console.WriteLine("false || true" + (false || true));
+        System.Console.WriteLine("true || false" + (true || false));
+        System.Console.WriteLine("false || false" + (false || false));
+        System.Console.WriteLine("!true" + (!true));
+        System.Console.WriteLine("!false" + (!false));
+        System.Console.WriteLine("!(!false)" + (!(!false)));
+        
+        
+
+        
+    }
+}
