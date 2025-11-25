3413dc7a335a5a3ee56918c9e9b31f9e47c8e3ba
marcoyimyaem <marcoyimyaem@gmail.com>
2025-11-25 12:48:32 +0800

    git ig

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed355550c473cd43d500cc56c8fe6e05512ca48b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9430da169381ad4e0fb1f6dbf248e81ef75db30f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
